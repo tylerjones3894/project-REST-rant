bf574288db38a826f2b19e885b534ac41492a549
Tyler Jones <tylerjones3894@gmail.com>
2023-01-27 11:19:15 -0800

    Adding 404 page view

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 
 // 404 PAGE
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.status(404).render('error404')
 })
 
 // LISTEN
---
+++ views/error404.jsx
@@ -0,0 +1,15 @@
+const React = require('react')
+const Def = require('./default')
+
+function error404() {
+    return (
+        <def>
+            <main>
+                <h1>404: PAGE NOT FOUND</h1>
+                <p>Oops, sorry, we can't find this page!</p>
+            </main>
+        </def>
+    )
+}
+
+module.exports = error404
