c83e6e0de5565752d7ed8596eefc34b9bb993ac3
Tyler Jones <tylerjones3894@gmail.com>
2023-02-12 19:58:53 -0800

    Seeding data, validations, and helper methods

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,13 +5,24 @@ function show () {
     return (
         <Def>
           <main>
-            <h1>{data.place.name}</h1>
-            <img src={place.image} alt={place.name} />
-          </main>
-          <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>  
-          <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-            <button type="submit" className="btn btn-danger">Delete</button>
-          </form>     
+            <div className="row">
+                <h1>{data.place.name}</h1>
+                <div className="col-sm-6">
+                    <img src={data.place.image} alt={data.place.name} />
+                    <h3>Located in {data.place.city}, {data.place.state}</h3>
+                </div>
+                <div className="col-sm-6">
+                    <h3>{data.place.showEstablished()}</h3>
+                    <h4>Serving {data.place.cuisine}</h4>
+                </div>
+                <div className="col-sm-6">
+                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>  
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                        <button type="submit" className="btn btn-danger">Delete</button>
+                    </form> 
+                </div>
+            </div>
+          </main>    
         </Def>
     )
 }
